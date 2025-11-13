2dffe12aaaaae3af8a50a64f42b80b62662ccccd
Johnson Worlanyo Ahiadorme <51439824+jahiadorme@users.noreply.github.com>
2025-11-13 11:12:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{const toggle=document.getElementById('dark-mode-toggle');toggle.addEventListener('click',()=>{document.body.classList.toggle('dark-mode');});});
