4dd6f1ba623c9ebbcde8fdfb97734206ac036b54
Johnson Worlanyo Ahiadorme <51439824+jahiadorme@users.noreply.github.com>
2025-11-14 12:52:09 +0000

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{const toggle=document.getElementById('dark-mode-toggle');toggle.addEventListener('click',()=>{document.body.classList.toggle('dark-mode');});});
